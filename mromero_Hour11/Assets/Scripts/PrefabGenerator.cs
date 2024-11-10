994bf2ef606800140b8367226fca554d5cd44ea2
TezRomero <amr262667@gmail.com>
2024-11-10 10:18:16 -0800

    Hour 11 P2

--- mromero_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ mromero_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
+using UnityEngine.VFX;
 
 public class PrefabGenerator : MonoBehaviour
 {
+    public GameObject streetLampPrefab;
+    public float distanceBetweenLamps = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 10; i++)
+        {
+            Vector3 position = new(i * distanceBetweenLamps, 0, 0);
+            Instantiate(streetLampPrefab, position, Quaternion.identity);
+        }
     }
     public GameObject prefab;
     // Update is called once per frame
