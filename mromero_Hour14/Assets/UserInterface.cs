f227fcba5481555f8b5fdfd04dbaa50d8c8eee41
TezRomero <amr262667@gmail.com>
2024-11-24 15:09:05 -0800

    Hour 14

---
+++ mromero_Hour14/Assets/UserInterface.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    // Start is called before the first frame update
+  /*
+   I changed the background for both the title screen and the level select screen
+   I made all the text black
+   I made the color change different colors and have it say start
+   I changed the font for both titles, the button, and the rest of the text 
+   I changed the font size dpepnding on the certain text I was using
+   */
+}
