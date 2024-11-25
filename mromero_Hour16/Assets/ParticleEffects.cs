c4e01c83bc1ea2ee95a7740681d3ed5ea5754217
TezRomero <amr262667@gmail.com>
2024-11-24 20:41:50 -0800

    Hour 16

---
+++ mromero_Hour16/Assets/ParticleEffects.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffects : MonoBehaviour
+{
+ /*
+ I changed the particle color overtime to have particles coming out at 2 different colors
+ I added a curve to the start time and particle size
+ I changed the start speed to 20
+ I changed the rotation of the particles
+ I changed the gravity as well
+ */
+}
