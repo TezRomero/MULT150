2942baedcc0a88abba96d7d55f0f505e7acab295
TezRomero <amr262667@gmail.com>
2024-12-11 13:54:37 -0800

    Hour 20

---
+++ mromero_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ I think the game is too easy because it's very basic so you don't really have to try and there is no way for the character to get killed so there is no effect of fear
+ I would allow the player to take damage and put more obstacles that you could possibly jump over
+ I think sliding/jumping/boosting and enemies with the player having a weapon would make a wow factor
+ I like the simple game style so you can play it anytime and (if it counts) I really enjoyed making it
+ The coding and then connecting all the different materials and textures to everything to connect it
+ */
---
+++ mromero_Hour20/Assets/Scripts/GameCritic1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic1 : MonoBehaviour
+{
+ /*
+ I made the object skinnier and I changed the color to purple
+
+I changed the particle from a cone to a hemisphere and changed the color of the particles and the color of the powerup
+ */
+}
