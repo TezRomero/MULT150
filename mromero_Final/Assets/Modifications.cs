110f1b437b3d1f9e91fdacb9b339761028274f33
TezRomero <amr262667@gmail.com>
2024-12-16 22:13:24 -0800

    Final

---
+++ mromero_Final/Assets/Modifications.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modifications : MonoBehaviour
+{
+/*
+ I changed the Intensity of the directional light and changed the color to a light blue giving the water a blue tint
+ Added Fog
+ For the skybox I changed the sun size to 0.5, the atmosphere thickness to 1.6, the sky tint to a light blue, and the exposure to 1.4
+ Chaos Balls: I made them into a prefab and added them to the scene. For the particle system I changed the color, made it into hemisphere shape and changed the rate over time. 
+ Coloured Balls: I made them into a prefab
+ Goals: I changed the colour intensity, and the range of them. I wanted to keep them the same color as the balls.
+ Wall: I made the walls into having pillars and middle wide walls with spikes coming out of the pillars. I also put a new texture with a checkered look to them. 
+ */
+}
--- mromero_Hour10/Assets/Scripts/Updates.cs
+++ mromero_Hour10/Assets/Scripts/Updates.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Updates : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+/*
+88        88                      88                                           888888888888             ,ad8888ba,  88                                            
+88        88                      88              ,d                                88                 d8"'    `"8b 88                                            
+88        88                      88              88                                88                d8'           88                                            
+88        88 8b,dPPYba,   ,adPPYb,88 ,adPPYYba, MM88MMM ,adPPYba, ,adPPYba,         88  ,adPPYba,     88            88,dPPYba,  ,adPPYYba,  ,adPPYba,  ,adPPYba,  
+88        88 88P'    "8a a8"    `Y88 ""     `Y8   88   a8P_____88 I8[    ""         88 a8"     "8a    88            88P'    "8a ""     `Y8 a8"     "8a I8[    ""  
+88        88 88       d8 8b       88 ,adPPPPP88   88   8PP"""""""  `"Y8ba,          88 8b       d8    Y8,           88       88 ,adPPPPP88 8b       d8  `"Y8ba,   
+Y8a.    .a8P 88b,   ,a8" "8a,   ,d88 88,    ,88   88,  "8b,   ,aa aa    ]8I         88 "8a,   ,a8"     Y8a.    .a8P 88       88 88,    ,88 "8a,   ,a8" aa    ]8I  
+ `"Y8888Y"'  88`YbbdP"'   `"8bbdP"Y8 `"8bbdP"Y8   "Y888 `"Ybbd8"' `"YbbdP"'         88  `"YbbdP"'       `"Y8888Y"'  88       88 `"8bbdP"Y8  `"YbbdP"'  `"YbbdP"'  
+             88                                                                                                                                                   
+             88             
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    // First thing I did was make a Chaos Ball Goal
+    // Second thing was change the size of the players bumper
+    // Third thing I did is making variations of the chaos ball
+    // Fourth thing I did was make a complex bumper out of many shapes
+
+
+
+
+
+
+*/
 }
